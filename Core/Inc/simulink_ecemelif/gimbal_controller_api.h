41fa0b4a078e601e99b1b84abb4a08ac882ee090
elifik <elif40tr@gmail.com>
2026-04-26 21:07:37 +0300

    v.0.3.206
    get set ekledik LRUD ve pitch search dahil ederek

--- Core/Inc/simulink_ecemelif/gimbal_controller_api.h
+++ Core/Inc/simulink_ecemelif/gimbal_controller_api.h
@@ -3,10 +3,19 @@
 
 #include "gimbal_controller.h"
 
-void gimbal_controller_set_Q1(real_T value);
+void gimbal_controller_set_L(real_T value);
+void gimbal_controller_set_R(real_T value);
+void gimbal_controller_set_U(real_T value);
+void gimbal_controller_set_D(real_T value);
 void gimbal_controller_set_theta_meas_yaw(real_T value);
+void gimbal_controller_set_theta_meas_pitch(real_T value);
 
 real_T gimbal_controller_get_omega_cmd_yaw(void);
+real_T gimbal_controller_get_omega_cmd_pitch(void);
 boolean_T gimbal_controller_get_beam_valid(void);
+real_T gimbal_controller_get_e_x(void);
+real_T gimbal_controller_get_e_y(void);
+boolean_T gimbal_controller_get_comm_enabled(void);
+real_T gimbal_controller_get_lock_counter(void);
 
 #endif /* GIMBAL_CONTROLLER_API_H_ */
--- Core/Src/simulink_ecemelif/gimbal_controller_api.c
+++ Core/Src/simulink_ecemelif/gimbal_controller_api.c
@@ -1,8 +1,23 @@
 #include "simulink_ecemelif/gimbal_controller_api.h"
 
-void gimbal_controller_set_Q1(real_T value)
+void gimbal_controller_set_L(real_T value)
 {
-    gimbal_controller_U.Q1 = value;
+    gimbal_controller_U.L = value;
+}
+
+void gimbal_controller_set_R(real_T value)
+{
+    gimbal_controller_U.R = value;
+}
+
+void gimbal_controller_set_U(real_T value)
+{
+    gimbal_controller_U.U = value;
+}
+
+void gimbal_controller_set_D(real_T value)
+{
+    gimbal_controller_U.D = value;
 }
 
 void gimbal_controller_set_theta_meas_yaw(real_T value)
@@ -10,12 +25,42 @@ void gimbal_controller_set_theta_meas_yaw(real_T value)
     gimbal_controller_U.theta_meas_yaw = value;
 }
 
+void gimbal_controller_set_theta_meas_pitch(real_T value)
+{
+    gimbal_controller_U.theta_meas_pitch = value;
+}
+
 real_T gimbal_controller_get_omega_cmd_yaw(void)
 {
     return gimbal_controller_Y.omega_cmd_yaw;
 }
 
+real_T gimbal_controller_get_omega_cmd_pitch(void)
+{
+    return gimbal_controller_Y.omega_cmd_pitch;
+}
+
 boolean_T gimbal_controller_get_beam_valid(void)
 {
     return gimbal_controller_Y.beam_valid;
+}
+
+real_T gimbal_controller_get_e_x(void)
+{
+    return gimbal_controller_Y.e_x;
+}
+
+real_T gimbal_controller_get_e_y(void)
+{
+    return gimbal_controller_Y.e_y;
+}
+
+boolean_T gimbal_controller_get_comm_enabled(void)
+{
+    return gimbal_controller_Y.comm_enabled;
+}
+
+real_T gimbal_controller_get_lock_counter(void)
+{
+    return gimbal_controller_Y.lock_counter;
 }
